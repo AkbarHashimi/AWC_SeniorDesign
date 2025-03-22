d8ee3c036054f502b2ec764c379699fa8d66ef80
Akbar Hashimi <snakelegendary@gmail.com>
2025-03-22 14:45:52 -0700

    updated function signatures for adc

--- adc.h
+++ adc.h
@@ -4,35 +4,87 @@
 /*
  
  * Considerations:
- * ADC interrupts must read from the buffer THEN clear AFTER the buffer is read.
-
+ * ADC interrupts must read from the buffer THEN clear the interrupt flag AFTER the buffer is read.
+ * Max sample speed is 400 ksps
+ * Sample time minimum is 200 ns
  
  
  */
 
 
-
+//Pre: ADC must be OFF
+//Post: Analog Pins that are used will be put into analog mode
 void configANPins();
+
+//Pre: ADC must be OFF
+//Post: Configures the two MUX inputs A and B
 void configIntoMux();
+
+//Pre: ADC must be OFF
+//Post: Configures the format of the result buffer - currently a 32 bit unsigned int format
+//with rightmost 10 bits being data, the rest is 0
 void setOutputFormat();
-void setSampleClockSource();
+
+//Pre: ADC must be OFF
+//Post: Sets Conversion to happen automatically
+void setSampleConvTrigger();
+
+//Pre: ADC must be OFF
+//Post: Sets Voltage Reference for HIGH and LOW for system
 void setVoltageRefs();
+
+//Pre: ADC must be OFF
+//Post: Sets input scan mode to OFF
 void selectScanMode();
+
+//Pre: ADC must be OFF
+//Post: Sets the rate of samples the interrupt should occur
+//in our case 2 Samples are converted then the interrupt happens
 void setConvPerInterrupt();
+
+//Pre: ADC must be OFF
+//Post: Buffer Size is set to either 8 bit or 16 bit mode - we are using 16 bit
 void setBufferFillMode();
-void setMuxToADCConnection();
-void setADCClockSource();
+
+
+//Pre: ADC must be OFF
+//Post: Configures the ADC Clock Source and Speed
+/*
+    
+ * Clock source is PBCLK
+ * TAD = 18*TPB 18 times the PBCLK - ADC Clock Speed
+ * Auto Sample Time bits
+ 
+ 
+ */
+void configADC_Clock();
+
+//Pre: ADC must be OFF
+//Post: sets sample time of ADC
 void setSampleTime();
-void setADCClockPrescaler();
+
+//Pre: ADC must be OFF
+//Post: sets the ADC to alternate between sampling TSENSE and CSENSE
 void setAlternateMode();
 
-void setupADC();
+//Pre: ADC must be OFF
+//Post: 
+void adc_init();
 
-void turnOnADC();
-void turnOffADC();
+//Pre: adc_init() should be called first to initialize config registers
+//Post: Turns on ADC
+void adc_on();
 
+//Pre: None
+//Post: Turns ADC on
+void adc_off();
 
+//Pre: ADC should be ON
+//Post: 
+void stopSampling();
 
-
+//Pre: ADC should be ON
+//Post: 
+void startSampling();
 
 #endif
